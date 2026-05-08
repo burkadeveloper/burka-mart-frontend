0e617732d1a70a7dfa984a0c397964cd8348d4c0
burka <burkaa16@gmail.com>
2026-05-08 19:58:44 +0300

    Frontend: Updated imageHelper for Cloudinary and fixed dependency crash

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 3000, proxy: { "/api": "http://localhost:5000" } },
+  server: {
+    port: 3000,
+    proxy: { "/api": "https://burka-mart-api.onrender.com" },
+  },
 });
